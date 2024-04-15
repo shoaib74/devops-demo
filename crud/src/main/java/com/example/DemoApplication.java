f36f708e11ed211870f06e2a265952879b1ae6b2
shoaib-adda247 <shoaib.ahmed@adda247.com>
2024-04-15 23:48:20 +0530

    dockerfile update

--- crud/src/main/java/com/example/DemoApplication.java
+++ crud/src/main/java/com/example/DemoApplication.java
@@ -2,10 +2,17 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication  extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
+
 }
